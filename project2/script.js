ebfde0f325dfb7fc49859d96668a8a28cb118879
tanay3110 <tanayawachat1603@gmail.com>
2026-07-02 00:15:01 +0530

    todo list completed

--- project2/script.js
+++ project2/script.js
@@ -1,8 +1,19 @@
-let inp = document.querySelector('input');
+form = document.querySelector("#todo-form");
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+});
 
-inp.addEventListener("click", add)
+    let inp = document.querySelector('#todo-input');
+    let btn = document.querySelector('#todo-form button');
 
-function add() {
+    btn.addEventListener("click", add);
 
-
-}
+    function add() {
+        let task = inp.value;
+        if (task.trim() !== "") {
+            let li = document.createElement("li");
+            li.textContent = task;
+            document.querySelector("#todo-list").appendChild(li);
+            inp.value = "";
+        }
+    };
